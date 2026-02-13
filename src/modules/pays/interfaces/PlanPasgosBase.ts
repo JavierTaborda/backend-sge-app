9d15538a713e3452ad4f93e840ab13e5a63a7d6e
JavierTaborda <javiert.develop@gmail.com>
2026-02-13 10:07:52 -0400

    feat: cahnge to number plan pago

--- src/modules/pays/interfaces/PlanPasgosBase.ts
+++ src/modules/pays/interfaces/PlanPasgosBase.ts
@@ -16,7 +16,7 @@ export interface PlanPagosBase {
   fechaemision: string | Date;
   fechavencimiento: string | Date;
   fecharegistro: string | Date;
-  numerofactura: string;
+  numerofactura: number ;
   observacion?: string | null;
   tasacambio: string | number;
   moneda: string;
